c8c56ce679d463d15ca076e4e48dab6b82362c0c
Admin <ge1983@yandex.ru>
2023-03-26 17:41:02 +1000

    Добавлена задача 1 для ДЗ

---
+++ DZ1/Program.cs
@@ -0,0 +1,59 @@
+﻿//  Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+
+int[,] InputArray (int length1, int length2)
+{
+    int[,] array = new int[length1, length2];
+    Random randon = new Random();
+    for (int i = 0; i < length1; i++)
+    {
+        for (int j = 0; j < length2; j++)
+        {
+            array[i,j] = randon.Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray (int[,] array)
+{
+    for (int i = 0; i<array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");  
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[,] SortArray (int[,] array)
+{
+    int z = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = array.GetLength(1)-1; j > 0; j--)
+        {
+            for (int k = 0; k < j; k++)
+            {
+                if (array[i,k] < array[i,k+1])
+                {
+                    z = array[i,k];
+                    array[i,k] = array[i,k+1];
+                    array[i,k+1] = z;
+
+                }
+            }
+        }
+    }
+    return array;
+}
+
+int[,] array = InputArray(7,7);
+
+Console.WriteLine("Полученный двумерный массив");
+PrintArray(array);
+
+Console.WriteLine("Отсортированный массив");
+PrintArray(SortArray (array));
